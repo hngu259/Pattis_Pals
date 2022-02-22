fb8b8d73abf326f2d9a5363a5fc30dce994a3096
Claire White <claireew02@gmail.com>
2022-02-21 23:07:03 -0600

    added machine and muscle classes

--- geaux_Urec_Main.java
+++ geaux_Urec_Main.java
@@ -1,23 +1,23 @@
 public class geaux_Urec_Main {
-    
+
     /**
      * This is the main method of the entire program:
      * 
      * Methods in this main class should run the program the following way:
-     *      -  Upon opening the program, the user will enter personal information
-     *          (ie. name, height, weigh, etc.)
-     *      -  The user will then be prompted to select what muscle group they would
-     *          like to work on for this workout.
-     *      -  Upon selecting a muscle group from the menu, machines related to that
-     *          muscle group will be shown.
-     *      -  When a machine is selected, machine name, location, and max load weight
-     *          will be displayed.
-     *      -  User should be able to navigate back and forth between muscle list
-     *          and machine list.
-     *      -  As the user gets through the workout, a queue will be created, storing 
-     *          machines used(possibly save what weight was lifted).
-     *      -  Once the user has completed their workout, a workout summary will be
-     *          displayed, showing machines used(possibly display what weight was
-     *          lifted).
+     * - Upon opening the program, the user will enter personal information
+     * (ie. name, height, weigh, etc.)
+     * - The user will then be prompted to select what muscle group they would
+     * like to work on for this workout.
+     * - Upon selecting a muscle group from the menu, machines related to that
+     * muscle group will be shown.
+     * - When a machine is selected, machine name, location, and max load weight
+     * will be displayed.
+     * - User should be able to navigate back and forth between muscle list
+     * and machine list.
+     * - As the user gets through the workout, a queue will be created, storing
+     * machines used(possibly save what weight was lifted).
+     * - Once the user has completed their workout, a workout summary will be
+     * displayed, showing machines used(possibly display what weight was
+     * lifted).
      */
 }
---
+++ machine.java
@@ -0,0 +1,8 @@
+/*
+    Creates each machine
+    Pulls data from machineData
+*/
+
+public class machine {
+
+}
