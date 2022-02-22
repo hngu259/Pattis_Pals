48124e22fc8e11a1e2471d3246b4a736ae7806b4
Hoai Nguyen <hnguye18@uno.edu>
2022-02-21 18:28:25 -0600

    geaux_Urec_barebones

---
+++ geaux_Urec_machineData.java
@@ -0,0 +1,15 @@
+public class geaux_Urec_machineData {
+    
+    /**
+     * This class will contain data on all the machines within the UREC:
+     *      - Machine Location
+     *      - Machine Name
+     *      - Max Machine Weight
+     *      - Machine description
+     * 
+     * Other pertinent information may be added to the list at a later time.
+     * This class will be called upon after the user has selected the 
+     * desired muscle group for their workout and display the relevant information
+     * above.
+     */
+}
---
+++ geaux_Urec_muscleData.java
@@ -0,0 +1,10 @@
+public class geaux_Urec_muscleData {
+    
+    /**
+     * This class will contain data on the different muscle groups in the body:
+     * 
+     * This simple class will allow the user to choose the particular muscle
+     * they would like to work on, followed by a call upon the 
+     * geaux_Urec_machineData class. 
+     */
+}
